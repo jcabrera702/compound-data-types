3ffb0f56fa38f2750dcbe6ac66f15519484fcf7d
Justin <justincrypto702@gmail.com>
2025-12-23 12:58:31 -0800

    Add video notes to readME

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
     let numbers: [i32; 4]= [1,2,3,4];
     println!("Array of numbers {:?}", numbers);
+    // let mix = [1, 2, "apple", true];
+    // println!("Mixed Array {:?}", mix);
+    let fruits: [&str; 3] = ["apple", "banana", "orange"];
+    println!("Fruits Array: {}", fruits[0]);
+    println!("Fruits Array: {}", fruits[1]);
+    println!("Fruits Array: {}", fruits[2]);
 }
---
+++ src/readME.md
@@ -0,0 +1,12 @@
+# 🚀 Notes
+
+Rust Basics 2024: Lesson 2 | Compound Data Types
+
+## ✨ Formats
+1.) In Rust there are 2 types of formats <br>
+-Debuggable Format <br>
+-Display Format
+
+##  &str is not a string
+ its a reference to a string 
+
